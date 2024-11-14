96e091425d05167fc50952bcabafd1c3e0e84122
zahra Ardalan <z.ardalan78@gmail.com>
2024-11-14 13:01:52 +0330

    edit7

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const searchInput = document.querySelector("#search");
+const productsDOM = document.querySelector(".products-center");
 
 let allproductsdata = [];
 const filters = {
@@ -18,11 +19,31 @@ function renderproducts(_products, _filters) {
   const filteredProducts = _products.filter((p) => {
     return p.title.toLowerCase().includes(_filters.searchItems.toLowerCase());
   });
+  productsDOM.innerHTML = "";
   console.log(filteredProducts);
+  //render to DOM
+  filteredProducts.forEach((item, index) => {
+    //create
+    //content
+    //append to products:
+    const productsDiv = document.createElement("div");
+    productsDiv.classList.add("product");
+    productsDiv.innerHTML = `
+    <div class="img-container">
+        <img src=${item.image} alt="p-${index}" />
+      </div>
+      <div class="product-desc">
+        <p class="product-price">${item.price}$</p>
+        <p class="product-title">${item.title}</p>
+
+      </div>`;
+    //append to DOM
+    productsDOM.appendChild(productsDiv);
+  });
 }
 
 searchInput.addEventListener("input", (e) => {
-  console.log(e.target.value);
+  //console.log(e.target.value);
   filters.searchItems = e.target.value;
   renderproducts(allproductsdata, filters);
 });
